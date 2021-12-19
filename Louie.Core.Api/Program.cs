195b81babce0c0739856e3e736d78539c2c65ab6
enes cinar <enescihancinar@gmail.com>
2021-12-18 23:02:02 -0800

    runtime info added

--- Louie.Core.Api/Program.cs
+++ Louie.Core.Api/Program.cs
@@ -1,3 +1,5 @@
+using System.Runtime.InteropServices;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -8,6 +10,26 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+#region Runtime Banner
+Console.ForegroundColor = ConsoleColor.DarkYellow;
+Console.WriteLine($@"---------------------------------
+LOUIE
+---------------------------------");
+Console.ResetColor();
+
+var urls = builder.WebHost
+    .GetSetting(WebHostDefaults.ServerUrlsKey)?
+    .Replace(";", " "); // make clickable
+Console.Write($"    Urls: ");
+Console.ForegroundColor = ConsoleColor.DarkCyan;
+Console.WriteLine($"{urls}", ConsoleColor.DarkCyan);
+Console.ResetColor();
+
+Console.WriteLine($" Runtime: {RuntimeInformation.FrameworkDescription} - {app.Environment.EnvironmentName}");
+Console.WriteLine($"Platform: {RuntimeInformation.OSDescription}");
+Console.WriteLine();
+#endregion
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
